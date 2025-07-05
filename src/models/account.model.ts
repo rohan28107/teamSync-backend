78e9ae0629115a1deae28d80569f015487be9feb
Rohan Gajare <rohangajre21@gmail.com>
2025-07-05 17:27:10 +0530

    added all model, created role seeder, uuid

--- src/models/account.model.ts
+++ src/models/account.model.ts
@@ -1,5 +1,5 @@
 import mongoose, { Document, Schema } from "mongoose";
-import { ProviderEnumType } from "../enums/account-provider.enum";
+import { ProviderEnum, ProviderEnumType } from "../enums/account-provider.enum";
 
 export interface AccountDocument extends Document {
   provider: ProviderEnumType;
@@ -9,3 +9,36 @@ export interface AccountDocument extends Document {
   tokenExpiry: Date | null;
   createdAt: Date;
 }
+
+const accountSchema = new Schema<AccountDocument>(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    provider: {
+      type: String,
+      enum: Object.values(ProviderEnum),
+      required: true,
+    },
+    providerId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    refreshToken: { type: String, default: null },
+    tokenExpiry: { type: Date, default: null },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform(doc, ret) {
+        delete ret.refreshToken;
+      },
+    },
+  }
+);
+
+const AccountModel = mongoose.model<AccountDocument>("Account", accountSchema);
+export default AccountModel;
